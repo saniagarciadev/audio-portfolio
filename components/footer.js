7c1489d3c861d21254d1c4056350e835fe0839b8
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-01-19 19:08:43 +0100

    add temp logo and change bg colors

--- components/footer.js
+++ components/footer.js
@@ -3,19 +3,10 @@ import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Footer() {
   return (
-    <footer className="bg-indigo-500">
+    <footer className="bg-[#3702E4]">
       <Container>
-        <div className="py-3 px-7 flex flex-row gap-10 items-center justify-end">
-          <p>Copyright © 2022 Bien Sûr. All Rights Reserved.</p>
-          <p>
-            Site by{" "}
-            <a
-              href="https://github.com/saniagarciadev"
-              className="underline hover:text-success duration-200 transition-colors"
-            >
-              Sania García
-            </a>
-          </p>
+        <div className="py-3 px-7 text-right flex flex-row flex-wrap gap-10 items-center justify-end">
+          <p>Copyright © 2023 Bien Sûr. All Rights Reserved.</p>
         </div>
       </Container>
     </footer>
--- components/intro.js
+++ components/intro.js
@@ -1,31 +1,34 @@
+import Image from "next/image";
 import { CMS_NAME, CMS_URL } from "../lib/constants";
+import logo from "../public/logo.png";
 
 export default function Intro() {
   return (
-    <section className="h-[50vh] py-10 px-10 bg-indigo-500 flex-col flex justify-between items-center">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="min-h-[100vh] py-10 px-10 bg-[#3702E4] flex-col flex justify-between items-center">
+      {/* <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         BIEN SÛR
-      </h1>
-
-      <p>Hi, I'm a very very thiq boi and i like to make the musicsss :D</p>
-      <div className="flex flex-row gap-10">
+      </h1> */}
+      <div className="w-[400px] max-w-[100vw]">
+        <Image src={logo} alt="Bien Sûr logo" />
+      </div>
+      <div className="my-20 text-[#3702E4] uppercase p-10 bg-black flex flex-col gap-10">
         <a
           href="https://soundcloud.com/biensurr"
-          className="underline hover:text-success duration-200 transition-colors"
+          className="hover:text-success duration-200 transition-colors"
         >
           Soundcloud
         </a>
         <a
-          href="https://www.instagram.com/lex_klein_/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Instagram
-        </a>
-        <a
           href="https://twitter.com/thickassdj"
-          className="underline hover:text-success duration-200 transition-colors"
+          className="hover:text-success duration-200 transition-colors"
         >
           Twitter
+        </a>
+        <a
+          href="https://www.instagram.com/lex_klein_/"
+          className="hover:text-success duration-200 transition-colors"
+        >
+          Instagram
         </a>
       </div>
     </section>
