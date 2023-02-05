77da81897acf2c4acb5fe27d592a9a2743e6eb72
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-02-05 12:25:47 +0100

    add bien sur boi

--- components/intro.js
+++ components/intro.js
@@ -1,18 +1,13 @@
 import Image from "next/image";
 import { CMS_NAME, CMS_URL } from "../lib/constants";
 import logo from "../public/logo.png";
+import boi from "../public/boi.png";
+import Spline from "@splinetool/react-spline";
 
 export default function Intro() {
   return (
-    <section className="min-h-[100vh] py-10 px-10 bg-[#3702E4]  flex-col flex justify-between items-center">
-      {/* <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        BIEN SÛR
-      </h1> */}
-      <div className="w-[100vw] flex justify-center items-center">
-        <div className="w-[700px] max-w-[100vw] m-8">
-          <Image src={logo} alt="Bien Sûr logo" />
-        </div>
-      </div>
+    <section className="min-h-[100vh] bg-[#3702E4]  flex-col flex justify-between items-center overflow-hidden">
+      <Spline scene="https://prod.spline.design/vIgYKAHpdvdu7qlX/scene.splinecode" />
       <div className="w-[100vw] py-20 text-[#3702E4] uppercase p-10  flex flex-col gap-10 justify-between items-center">
         <a
           href="https://soundcloud.com/biensurr"
