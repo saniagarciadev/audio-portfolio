5a8c588ec87147ce70705d8602888b2fb2774281
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-06-11 22:01:15 +0200

    add icons to navbar

--- components/header.js
+++ components/header.js
@@ -1,13 +1,29 @@
 import Link from "next/link";
 import NavButton from "./nav-button";
+import Image from "next/image";
+import soundcloudIcon from "../public/soundcloud-icon.svg";
+import igIcon from "../public/ig-icon.svg";
+import twitterIcon from "../public/twitter-icon.svg";
 
 export default function Header() {
   return (
     <nav className="relative w-fit mx-auto z-10 p-10  flex gap-4">
-      <NavButton route="/">:)</NavButton>
-      <NavButton route="https://soundcloud.com/biensurr">Soundcloud</NavButton>
-      <NavButton route="https://www.instagram.com/lex_klein_">Instagram</NavButton>
-      <NavButton route="https://twitter.com/thickassdj">Twitter</NavButton>
+      {/* <NavButton route="/">:)</NavButton> */}
+      <NavButton route="https://soundcloud.com/biensurr"><Image
+      priority
+      src={soundcloudIcon}
+      alt="Soundcloud"
+    /></NavButton>
+      <NavButton route="https://www.instagram.com/lex_klein_"><Image
+      priority
+      src={igIcon}
+      alt="Instagram"
+    /></NavButton>
+      <NavButton route="https://twitter.com/thickassdj"><Image
+      priority
+      src={twitterIcon}
+      alt="Twitter"
+    /></NavButton>
     </nav>
   );
 }
